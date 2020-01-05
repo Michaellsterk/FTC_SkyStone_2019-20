86f7c3e8badd2e5cd220c030cec6acd013ece748
Michaellsterk <34323970+Michaellsterk@users.noreply.github.com>
2020-01-05 15:08:54 -0500

    Made the robot actually work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompOpModes/CompRobot.java
@@ -0,0 +1,165 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.CompOpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Comp Robot", group="Linear Opmode")
+//@Disabled
+public class CompRobot extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFront = null;
+    private DcMotor rightFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightBack = null;
+    private Servo foundGrabber = null;
+
+    // declare motor speed variables
+    double RF; double LF; double RR; double LR;
+    // declare joystick position variables
+    double X1; double Y1; double X2; double Y2;
+    // operational constants
+    double joyScale = 0.5;
+    double motorMax = 0.6; // Limit motor power to this value for Andymark RUN_USING_ENCODER mode
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftFront  = hardwareMap.get(DcMotor.class, "fl_motor");
+        leftBack  = hardwareMap.get(DcMotor.class, "bl_motor");
+        rightFront = hardwareMap.get(DcMotor.class, "fr_motor");
+        rightBack = hardwareMap.get(DcMotor.class, "br_motor");
+        foundGrabber = hardwareMap.get(Servo.class, "found_servo");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+
+            // Reset speed variables
+            LF = 0; RF = 0; LR = 0; RR = 0;
+
+            // Get joystick values
+            Y1 = -gamepad1.right_stick_y * joyScale; // invert so up is positive
+            X1 = gamepad1.right_stick_x * joyScale;
+            Y2 = -gamepad1.left_stick_y * joyScale; // Y2 is not used at present
+            X2 = gamepad1.left_stick_x * joyScale;
+
+            // Forward/back movement
+            LF += Y1; RF += Y1; LR += Y1; RR += Y1;
+
+            // Side to side movement
+            LF += X1; RF -= X1; LR -= X1; RR += X1;
+
+            // Rotation movement
+            LF += X2; RF -= X2; LR += X2; RR -= X2;
+
+            // Clip motor power values to +-motorMax
+            LF = Math.max(-motorMax, Math.min(LF, motorMax));
+            RF = Math.max(-motorMax, Math.min(RF, motorMax));
+            LR = Math.max(-motorMax, Math.min(LR, motorMax));
+            RR = Math.max(-motorMax, Math.min(RR, motorMax));
+
+            // Send values to the motors
+            leftFront.setPower(LF);
+            rightFront.setPower(RF);
+            leftBack.setPower(LR);
+            rightBack.setPower(RR);
+
+            if(gamepad1.a) {
+                foundGrabber.setPosition(0.7);
+            }
+            else if(gamepad1.b) {
+                foundGrabber.setPosition(-0.7);
+            }
+            if(gamepad1.right_trigger>=0.5) {
+                joyScale = 0.25;
+            }
+            else if(gamepad1.x && gamepad1.y) {
+                joyScale = 1;
+            }
+            else {
+                joyScale = 0.5;
+            }
+
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("PowerScale", "Current Scaling: (%.1f)", joyScale);
+            //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
+        }
+    }
+}
